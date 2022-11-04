32076fbb8a28ae605bb7e6151d36dbcdbfd703ce
Lucas Costa Da Silva <lucasc@brq.com>
2022-11-04 14:01:51 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmesComponent } from './filmes/filmes.component';
+import { GeneroComponent } from './genero/genero.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', redirectTo:'home', pathMatch:'full'},
+{path:'home', component: HomeComponent},
+{path:'header', component: HeaderComponent},
+{path:'genero', component: GeneroComponent},
+{path:'filmes', component: FilmesComponent},
+{path:'usuario', component: UsuarioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatInputModule } from '@angular/material/input';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { MatButtonModule } from '@angular/material/button';
+import { FilmesComponent } from './filmes/filmes.component';
+import { MatSelectModule } from '@angular/material/select';
+import { GeneroComponent } from './genero/genero.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    UsuarioComponent,
+    FilmesComponent,
+    GeneroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
